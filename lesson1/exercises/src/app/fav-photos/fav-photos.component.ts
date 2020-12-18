a6c2f01c07a5d8a0a74ffb0e0e95561f4d9dfb83
Michaela Goldsmith <megold12@yahoo.com>
2020-12-17 21:04:20 -0600

    updated images and css

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.imgur.com/8u4eweH.jpg';
+  image3 = 'https://64.media.tumblr.com/ce3a52328ebb8c89e1a5cd7649468216/tumblr_inline_pmof1giIRI1ud0rrx_640.jpg';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies: string[] = ['The Manchurian Candidate', 'Oceans 8', 'Uncle Frank', 'The Prom', 'Jingle Jangle', 'Happiest Season', 'A Series of Unfortunate Events'];
 
    constructor() { }
 
